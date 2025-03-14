ef11ad190de3016c04d764db141c72226e3055e3
BytePudding <developerjieun94@gmail.com>
2025-03-15 07:41:32 +0900

    finished development for calculator

---
+++ src/constants/constants.js
@@ -0,0 +1,12 @@
+/**
+ * 투자 계산기에 사용될 초기 투자 항목 리스트
+ * - 초기 투자금, 연간 투자금, 기대 수익률, 투자 기간
+ */
+const initialInvestmentDetails = [
+  { id: "initialInvestment", label: "INITIAL INVESTMENT", amount: "" },
+  { id: "annualInvestment", label: "ANNUAL INVESTMENT", amount: "" },
+  { id: "expectedReturn", label: "EXPECTED RETURN", amount: "" },
+  { id: "duration", label: "DURATION", amount: "" },
+];
+
+export { initialInvestmentDetails };
