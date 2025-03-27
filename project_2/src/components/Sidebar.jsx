a3fb5f6a7eec555270cf4249f870089b6699af9d
kyoungjooo <leekjoo1008@gmail.com>
2025-03-27 23:00:49 +0900

    feat: 프로젝트 추가 확인

--- project_2/src/components/Sidebar.jsx
+++ project_2/src/components/Sidebar.jsx
@@ -8,13 +8,13 @@ export default function Sidebar({ todoList }) {
 
   const handleUpdateCurrentTodo = (todoId) => {
     //클릭한 투두와 아이디가 일치하는 투두 활성화, 이동
-    const currentTodo = todoList.find((todo) => todo.id === todoId);
+    const currentTodo = todoList?.find((todo) => todo.id === todoId);
     setActive(currentTodo.id);
     navigate(`/${currentTodo.id}`);
   };
 
   useEffect(() => {
-    setActive(param.id ? param.id : todoList[0].id);
+    setActive(param.id ? param.id : todoList[0]?.id);
   }, [param.id]);
 
   return (
