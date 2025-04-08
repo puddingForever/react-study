e0f8fc2a29b266d9f5f24585439f1b8d15537499
Gyeongmin Kim <mumukim.game@gmail.com>
2025-04-08 20:54:34 +0900

    섹션 12: Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,44 @@
-import { useEffect, useState } from "react";
-import ProjectList from "./components/projects/ProjectList";
-import ProjectDetail from "./components/projects/ProjectDetail";
+function App() {
+/*
+퀴즈 앱
 
-// 페이지 구조잡기 및 프로젝트 관리 
-const App = () => {
-  const [projects, setProjects] = useState([]);
-  const [selectedProject, setSelectedProject] = useState(projects.length > 0 ? projects[0] : null);
-  const [showForm, setShowForm] = useState(false);
+1. 요구사항
+타이머
+- 지정한 시간만큼 게이지 바가 감소
+- 답을 고르면 노란색으로 변경되어 빠르게 감소
+- 정답을 체크한 후 원래의 색으로 돌아와 빠르게 감소
 
-  useEffect(() => {
-    if (projects.length > 0) {
-      setSelectedProject(projects[projects.length - 1]);
-    } else {
-      setSelectedProject(null);
-    }
-  }, [projects]);
+문제
+- 문제와 보기 4개
+- hover시 보기가 보라색으로 변경
+- 답을 고르면 해당 보기만 노란색으로 변경
+- 정답 체크 후 맞으면 초록색, 틀리면 빨간색으로 변경
 
-  // 프로젝트 추가 버튼 클릭시
-  const handleAddProject = () => {
-    setSelectedProject(null);
-    setShowForm(true);
-  };
+결과
+- 문제를 다 풀면 결과 페이지로 이동
+- 모든 문제에 대해 SKIPPED, ANSWERED CORRECTLY, ANSWERED INCORRECTLY를 %로 제공
+- 하단에는 1번부터 문제와 내가 선택한 답을 보여줌. 맞은 답은 초록색, 틀린 답은 빨간색으로 출력
 
-  // 프로젝트 선택시
-  const handleSelectProject = (project) => {
-    setSelectedProject(project);
-    setShowForm(false);
-  };
+2. 로직
+지정한 수 만큼 문제를 풀면 게임 종료
+문제당 풀이시간, 정답체크 시간, 정답 확인 시간을 제공
+게임 종료 후 결과 페이지로 이동
+문제에는 스킵이 없는데, 결과에 SKIPPED가 있는걸로 보아 풀지 않으면 스킵으로 처리되는듯
 
-  // 프로젝트 저장 버튼 클릭시
-  const handleSaveProject = (project) => {
-    const newProject = { ...project, id: Date.now().toString(), tasks: [] };
-    setProjects(prevProjects => [...prevProjects, newProject]);
-    setShowForm(false);
-  };
-
-  // 프로젝트 삭제 버튼 클릭시
-  const handleDeleteProject = (projectId) => {
-    const updatedProjects = projects.filter(project => project.id !== projectId);
-    setProjects(updatedProjects);
-  };
-
-  // 프로젝트의 task 업데이트
-  const handleUpdateTasks = (projectId, tasks) => {
-    setProjects(projects.map(project =>
-      project.id === projectId ? { ...project, tasks } : project
-    ));
-
-    setSelectedProject({ ...selectedProject, tasks });
-  };
-
-  return (
-    <div className="flex h-screen">
-      <ProjectList
-        projects={projects}
-        selectedProject={selectedProject}
-        onAddProject={handleAddProject}
-        onSelectProject={handleSelectProject}
-      />
-
-      <ProjectDetail
-        selectedProject={selectedProject}
-        showForm={showForm}
-        onSave={handleSaveProject}
-        onCancel={() => setShowForm(false)}
-        onDelete={handleDeleteProject}
-        onUpdateTasks={handleUpdateTasks}
-        onCreateNew={handleAddProject}
-      />
-    </div>
-  );
-};
+3. 구조 설계 - 임시
+App : context로 퀴즈, 퀴즈 상태 관리. 현재 화면 결정
+Header : 헤더
+LastTry : 이전 시도에 대한 정보
+Quiz : 질문과 답변 목록 관리
+- Question : 현재 질문. 진행 바도 포함되는듯
+-- Answers : 답변 목록
+- ProgressBar : 진행 바
+Summary : 결과 페이지
+- SummaryStats : 결과 통계
+QuizContext : 퀴즈 상태 관리
+useQuiz : 퀴즈 관련 로직
+questions : 퀴즈 목록
+*/
+}
 
 export default App;
