d52f9f29a044d2ad76f304394002df0bd65b3535
kyoungjooo <leekjoo1008@gmail.com>
2025-04-10 22:37:25 +0900

    feat: 타이머 생성

--- project_3/src/components/Quiz.jsx
+++ project_3/src/components/Quiz.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import QUESTION from "../question.js";
 import CompleteImage from "../assets/quiz-complete.png";
+import Timer from "./Timer.jsx";
 
 export default function Quiz() {
   //현재 활성화된 질문을 관리하고 다른 질문으로 변경
+  //답변이 선택될 때마다 재랜더링
   const [userAnswer, setUserAnswer] = useState([]);
   const activeQuertionIndex = userAnswer.length;
 
+  //답변을 클릭하면 선택한 답변이 목록에 쌓인다.
   const handleGetSelectAnswer = (answer) => {
     setUserAnswer((prev) => {
       return [...prev, answer];
@@ -29,6 +32,11 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <Timer
+          key={activeQuertionIndex}
+          timeout={10000}
+          onTimeout={() => handleGetSelectAnswer(null)}
+        />
         <h2>{QUESTION[activeQuertionIndex].text}</h2>
         <ul id="answers">
           {sufferedAnswers.map((question, idx) => {
---
+++ project_3/src/components/Timer.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export default function Timer({ timeout, onTimeout }) {
+  //남은 시간
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  //timeout 의 시간이 지나면 onTimeout 함수를 실행
+  //다음 문제로 넘기기
+  useEffect(() => {
+    console.log("timer 생성");
+    const timer = setTimeout(onTimeout, timeout);
+    return () => clearTimeout(timer);
+  }, [onTimeout, timeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      //10ms마다 remainingTime을 10ms씩 줄여서 남은 시간 관리
+      setRemainingTime((prev) => prev - 10);
+    }, 10);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <progress id="question-time" value={remainingTime} max={timeout} />;
+}
