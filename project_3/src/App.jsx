2fea89a3172023b2101409635e0087c2725ced9d
kyoungjooo <leekjoo1008@gmail.com>
2025-04-12 23:34:46 +0900

    refector: 전역 변수로 변경

--- project_3/src/App.jsx
+++ project_3/src/App.jsx
@@ -1,14 +1,59 @@
+import { useCallback, useState } from "react";
 import Header from "./components/Header";
 import Quiz from "./components/Quiz";
+import { AnswerContext } from "./context/answerContext";
+import QUESTION from "./question.js";
 
 function App() {
+  const [userAnswer, setUserAnswer] = useState([]);
+  const [answerState, setAnswerState] = useState("");
+  let activeIndex = 0;
+  //아직고른 답이 없으면 0 있으면 등록된 답변 수-1(현재 인덱스)
+
+  const handleGetSelectAnswer = useCallback(
+    function handleGetSelectAnswer(selectedAnswer) {
+      //넘어가기 전 선택된 답변의 색상 변경 로직 추가
+      setAnswerState("answered");
+      //답변을 클릭하면 선택한 답변이 목록에 쌓인다.
+      setUserAnswer((prev) => {
+        return [...prev, selectedAnswer];
+      });
+      setTimeout(() => {
+        if (selectedAnswer === QUESTION[activeIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [activeIndex]
+  );
+
+  if (activeIndex === QUESTION.length) {
+    //함수 실행 종료
+    return (
+      <div id="summary">
+        <img src={CompleteImage} alt="CompleteImage" />
+      </div>
+    );
+  }
+  const answerValue = {
+    userAnswer,
+    answerState,
+    activeIndex: answerState === "" ? userAnswer.length : userAnswer.length - 1,
+    handleGetSelectAnswer,
+  };
+
   return (
-    <>
+    <AnswerContext.Provider value={answerValue}>
       <Header />
       <main>
         <Quiz />
       </main>
-    </>
+    </AnswerContext.Provider>
   );
 }
 
--- project_3/src/components/Quiz.jsx
+++ project_3/src/components/Quiz.jsx
@@ -1,66 +1,18 @@
-import { useCallback, useState } from "react";
-import QUESTION from "../question.js";
-import CompleteImage from "../assets/quiz-complete.png";
-import Timer from "./Timer.jsx";
+import { useContext } from "react";
+import Card from "./Card.jsx";
+import { AnswerContext } from "../context/answerContext.jsx";
 
 export default function Quiz() {
+  const { activeIndex } = useContext(AnswerContext);
   //현재 활성화된 질문을 관리하고 다른 질문으로 변경
   //답변이 선택될 때마다 재랜더링
-  const [userAnswer, setUserAnswer] = useState([]);
-  const activeQuertionIndex = userAnswer.length;
-
-  //답변을 클릭하면 선택한 답변이 목록에 쌓인다.
-  const handleGetSelectAnswer = useCallback((answer) => {
-    setUserAnswer((prev) => {
-      return [...prev, answer];
-    });
-  }, []);
-
-  if (activeQuertionIndex === QUESTION.length) {
-    //함수 실행 종료
-    return (
-      <div id="summary">
-        <img src={CompleteImage} alt="CompleteImage" />
-      </div>
-    );
-  }
-  const sufferedAnswers = [...QUESTION[activeQuertionIndex].answers];
-  sufferedAnswers.sort(() => {
-    return Math.random() - 0.5;
-  });
 
   //자식 컴포넌트가 받는 props가 "새 객체/새 함수"면 부모, 자식 다시 리랜더링
   //useCallback 통해 같은 함수 참조 유지
-  const handleSkipAnswer = useCallback(
-    () => handleGetSelectAnswer(null),
-    [handleGetSelectAnswer]
-  );
+
   return (
     <div id="quiz">
-      <div id="question">
-        {/* 
-        부모 컴포넌트가 리랜더링 되면 자식(Timer)의 props를 이전과 비교
-        timeout=10000 이전과 같음, handleSkipAnswer useCallback 으로 이전과 같음
-        → 메모리 안 객체가 유지되면서 useEffect 내부 실행 X
-        */}
-        <Timer
-          key={activeQuertionIndex}
-          timeout={10000}
-          onTimeout={handleSkipAnswer}
-        />
-        <h2>{QUESTION[activeQuertionIndex].text}</h2>
-        <ul id="answers">
-          {sufferedAnswers.map((question, idx) => {
-            return (
-              <li key={idx} className="answer">
-                <button onClick={() => handleGetSelectAnswer(question)}>
-                  {question}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <Card key={activeIndex} />
     </div>
   );
 }
