b5ec9f8ee1fed94a5bed7b2ed162533d14e91407
kyoungjooo <leekjoo1008@gmail.com>
2025-05-13 14:38:13 +0900

    chore: 리덕스 툴킷, 리덕스 설치

--- project_4/src/components/MenuCard.jsx
+++ project_4/src/components/MenuCard.jsx
@@ -1,15 +1,24 @@
 import { BASE_URL } from "../api/base";
+
+/**
+ * 메뉴 카드 UI 컴포넌트
+ * @param {Object} menuData - 메뉴 데이터
+ * @param {Function} handleAddtoCart - 장바구니 담기
+ */
+
 export default function MenuCard({ menuData }) {
   const { id, description, image, name, price } = menuData;
 
+  const handleAddtoCart = () => {};
   return (
     <div className="meal-item">
-      <article>
-        <img src={`${BASE_URL}/${image}`} alt={name} />
-        <h3>{name}</h3>
-        <span className="meal-item-price">{price}</span>
-        <p className="meal-item-description">{description}</p>
-      </article>
+      <img src={`${BASE_URL}/${image}`} alt={name} />
+      <h3>{name}</h3>
+      <span className="meal-item-price">$ {price}</span>
+      <p className="meal-item-description">{description}</p>
+      <button className="meal-item-actions" onClick={handleAddtoCart}>
+        add To Cart
+      </button>
     </div>
   );
 }
--- project_4/src/components/MenuList.jsx
+++ project_4/src/components/MenuList.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 import MenuCard from "./MenuCard";
 import { BASE_URL } from "../api/base";
+
 /**
  * 메뉴 리스트를 렌더링하는 컴포넌트
  *
  */
+
 export default function MenuList() {
+  //메뉴별 데이터 목록
   const [meals, setMeals] = useState([]);
+
+  //서버에서 메뉴별 데이터 불러오기
   useEffect(() => {
     const getMealsData = async () => {
       const res = await fetch(`${BASE_URL}/meals`);
@@ -18,6 +23,7 @@ export default function MenuList() {
     };
     getMealsData();
   }, []);
+
   return (
     <div id="meals">
       {meals.map((meal) => {
