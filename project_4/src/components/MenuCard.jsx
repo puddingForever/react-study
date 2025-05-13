48eb9148eaa5482455b1998bfa46434a90df5d59
kyoungjooo <leekjoo1008@gmail.com>
2025-05-13 14:22:52 +0900

    공통 url 경로 분리

--- project_4/src/components/MenuCard.jsx
+++ project_4/src/components/MenuCard.jsx
@@ -1,10 +1,11 @@
+import { BASE_URL } from "../api/base";
 export default function MenuCard({ menuData }) {
   const { id, description, image, name, price } = menuData;
 
   return (
     <div className="meal-item">
       <article>
-        <img src={`http://localhost:3000/${image}`} alt={name} />
+        <img src={`${BASE_URL}/${image}`} alt={name} />
         <h3>{name}</h3>
         <span className="meal-item-price">{price}</span>
         <p className="meal-item-description">{description}</p>
--- project_4/src/components/MenuList.jsx
+++ project_4/src/components/MenuList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import MenuCard from "./MenuCard";
+import { BASE_URL } from "../api/base";
 /**
  * 메뉴 리스트를 렌더링하는 컴포넌트
  *
@@ -8,7 +9,7 @@ export default function MenuList() {
   const [meals, setMeals] = useState([]);
   useEffect(() => {
     const getMealsData = async () => {
-      const res = await fetch("http://localhost:3000/meals");
+      const res = await fetch(`${BASE_URL}/meals`);
       if (!res.ok) {
         throw new Error("데이터를 불러오지 못했음");
       }
