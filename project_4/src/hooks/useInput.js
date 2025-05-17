6d81b5ad47c45cce5d8564b635478dfb6af32265
kyoungjooo <leekjoo1008@gmail.com>
2025-05-17 18:40:11 +0900

    fix: 안쓰는 코드, 파일 삭제

--- project_4/src/hooks/useInput.js
+++ project_4/src/hooks/useInput.js
@@ -2,17 +2,13 @@ import { useState } from "react";
 
 export function useInput(defaultValue = "") {
   const [enteredValue, setEnteredValue] = useState(defaultValue);
-  const [didEdit, setDidEdit] = useState(false);
 
   const handleInputChange = (e) => {
     setEnteredValue(e.target.value);
   };
-  const handleInputBlur = () => {
-    setDidEdit(true);
-  };
+
   return {
     value: enteredValue,
     handleInputChange,
-    didEdit,
   };
 }
